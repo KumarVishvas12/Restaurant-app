01ff1c6371168c91562e83151e1f97de47850fa8
Your Name <you@example.com>
2023-11-23 14:44:48 +0530

    Fteched data from swiggy website and rendered

---
+++ src/components/Shimmer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Shimmer.css'
+
+export default function Shimmer() {
+  return (
+    <div className='shimmer-box-container'>
+      <div className="box" ></div>
+      <div className="box" ></div>
+      <div className="box" ></div>
+      <div className="box" ></div>
+      <div className="box" ></div>
+      <div className="box" ></div>
+      <div className="box" ></div>
+      <div className="box" ></div>
+    </div>
+  )
+}
